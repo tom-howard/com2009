90e269f74832bd65b46b8a04b5f590c1b2ace163
tom-howard <58332432+tom-howard@users.noreply.github.com>
2021-01-28 17:57:59 +0000

    Update rosbackup.sh

--- utils/rosbackup.sh
+++ utils/rosbackup.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 mkdir -p /mnt/u/wsl-ros
 
-touch ~/wsl-ros-log.txt
-
-echo "$(date): Backup from $(hostname)" >> ~/wsl-ros-log.txt
-
-cp ~/wsl-ros-log.txt /mnt/u/wsl-ros/wsl-ros-log.txt
+touch /mnt/u/wsl-ros/wsl-ros-log.txt
+echo "$(date): Backup from $(hostname)" >> /mnt/u/wsl-ros/wsl-ros-log.txt
 
 tar --exclude='/home/student/.local' \
     --exclude='/home/student/.cache' \
