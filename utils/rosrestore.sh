c49ce5b0158c1961a4ca1a855a906517db92fadb
tom-howard <58332432+tom-howard@users.noreply.github.com>
2021-01-28 17:59:00 +0000

    Update rosrestore.sh

--- utils/rosrestore.sh
+++ utils/rosrestore.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
+echo "$(date): Restore from $(hostname)" >> /mnt/u/wsl-ros/wsl-ros-log.txt
+
 tar -xvjf /mnt/u/wsl-ros/ros-backup.tar.gz -C /
-
-echo "$(date): Restore from $(hostname)" >> ~/wsl-ros-log.txt
-
-cp ~/wsl-ros-log.txt /mnt/u/wsl-ros/wsl-ros-log.txt
