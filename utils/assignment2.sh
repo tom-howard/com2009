427dc7b9a7e74fe21d296654cce73cc9e72f460e
tom-howard <58332432+tom-howard@users.noreply.github.com>
2021-04-29 09:56:36 +0100

    Update assignment2.sh

--- utils/assignment2.sh
+++ utils/assignment2.sh
@@ -5,13 +5,13 @@ git clone https://github.com/tom-howard/com2009_actions.git
 git clone https://github.com/tom-howard/com2009_vision.git
 git clone https://github.com/tom-howard/com2009_simulations.git
 
-cd ./com2009_actions/src/
+cd ~/catkin_ws/src/com2009_actions/src/
 chmod +x camera_sweep.py
 
-cd ./com2009_vision/src/
+cd ~/catkin_ws/src/com2009_vision/src/
 chmod +x image_colours.py colour_search.py
 mkdir -p ~/.gazebo/models/
-cd ../models/
+cd ~/catkin_ws/src/com2009_vision/models/
 cp -r ./line_following_track ~/.gazebo/models/
 
 cd ~/catkin_ws/ && catkin_make
