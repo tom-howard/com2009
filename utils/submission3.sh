9dc6e501871cf0fbbc9ed3e4d99767a7712cbb64
tom-howard <58332432+tom-howard@users.noreply.github.com>
2021-06-15 09:34:21 +0100

    Create submission3.sh

---
+++ utils/submission3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ~/catkin_ws/src/
+git clone https://github.com/tom-howard/com2009_actions.git
+git clone https://github.com/tom-howard/com2009_vision.git
+git clone https://github.com/tom-howard/com2009_simulations.git
+
+cd ~/catkin_ws/src/com2009_actions/src/
+chmod +x camera_sweep.py
+
+cd ~/catkin_ws/src/com2009_vision/src/
+chmod +x image_colours.py colour_search.py
+mkdir -p ~/.gazebo/models/
+cd ~/catkin_ws/src/com2009_vision/models/
+cp -r ./line_following_track ~/.gazebo/models/
+
+cd ~/catkin_ws/ && catkin_make
+
+mkdir -p /mnt/u/com2009_assignment2/task5/
