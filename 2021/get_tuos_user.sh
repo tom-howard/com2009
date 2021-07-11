a5068ed8f65ddff3788776455d72cdecfa26bab7
tom-howard <58332432+tom-howard@users.noreply.github.com>
2021-07-11 11:11:29 +0100

    Update get_tuos_user.sh

--- 2021/get_tuos_user.sh
+++ 2021/get_tuos_user.sh
@@ -2,6 +2,6 @@
 
 cd /mnt/c/ && cmd.exe /c "echo %USERNAME%" > ~/.wsl-ros/tuos_user
 dos2unix -q ~/.wsl-ros/tuos_user
-
+cd ~
 read -r first_line < ~/.wsl-ros/tuos_user
 export TUOS_USER=$first_line
