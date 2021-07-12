3e4a9b2c91730f216b7e6a6e565d9480aed9c538
tom-howard <58332432+tom-howard@users.noreply.github.com>
2021-07-12 11:27:19 +0100

    Update rosbackup.sh

--- 2021/rosbackup.sh
+++ 2021/rosbackup.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-echo "Backing up your WSL-ROS environment to 'U:\wsl-ros\ros-backup.tar.gz', please wait..."
+rbpth=$rbname'.tar.gz'
+rbwpth=$(sed "s/\/mnt\/u/U\:/;s/\//\\\/g" <<< $rbpth)
 
-mkdir -p /mnt/u/wsl-ros
+echo "Backing up your WSL-ROS environment to '"$rbwpth"', please wait..."
 
-touch /mnt/u/wsl-ros/wsl-ros-log.txt
+rbdir=$(sed "s/\/[^/]*$//" <<< $rbname)
 
-echo "$(date): Backup from $(hostname) [wsl-ros version: $WSL_ROS_VER]" >> /mnt/u/wsl-ros/wsl-ros-log.txt
+mkdir -p $rbdir
+
+rblog=$rbname'-log.txt'
+
+touch $rblog
+
+echo "$(date): Backup from $(hostname) [wsl-ros version: $WSL_ROS_VER]" >> $rblog
 
 tar --exclude='home/student/.local'    \
     --exclude='home/student/.cache'    \
     --exclude='home/student/.config'   \
     --exclude='home/student/pkgs'      \
     --exclude='home/student/.wsl-ros'  \
-    -cjf /mnt/u/wsl-ros/ros-backup.tar.gz -C / home/student/
+    -cjf $rbpth -C / home/student/
     
 echo "Backup complete."
