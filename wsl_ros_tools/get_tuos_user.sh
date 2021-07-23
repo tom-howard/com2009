16dac263bcf4126ba982333e7c3cf9a42bf3af5a
tom-howard <58332432+tom-howard@users.noreply.github.com>
2021-07-23 16:25:27 +0100

    preserve the filesystem location when called

--- wsl_ros_tools/get_tuos_user.sh
+++ wsl_ros_tools/get_tuos_user.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+cur_dir=$(pwd)
 cd /mnt/c/ && cmd.exe /c "echo %USERNAME%" > ~/.wsl-ros/tuos_user
 dos2unix -q ~/.wsl-ros/tuos_user
-cd ~
+cd $cur_dir
 read -r first_line < ~/.wsl-ros/tuos_user
 export TUOS_USER=$first_line
 rm ~/.wsl-ros/tuos_user
