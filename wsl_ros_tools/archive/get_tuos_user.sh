120478082ad7a6e0d89c04aebb14693b1223b012
Tom Howard <t.howard@sheffield.ac.uk>
2021-12-20 15:31:15 +0000

    Removed wsl_ros_tools
    Transferred to an alternative repo

--- wsl_ros_tools/archive/get_tuos_user.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# this doesn't currently work! (something to do with a trailing LF in the ~/tuos_user file maybe?)
-
-cd /mnt/c/
-
-cmd.exe /c "echo %USERNAME%" > ~/tuos_user
-dos2unix -q ~/tuos_user
-export TUOS_USER=$(cat ~/tuos_user)
--- wsl_ros_tools/archive/rosrestore.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "$(date): Restore to $(hostname) [wsl-ros version: $WSL_ROS_VER]" >> /mnt/u/wsl-ros/wsl-ros-log.txt
-
-tar -xvjf /mnt/u/wsl-ros/ros-backup.tar.gz -C /
