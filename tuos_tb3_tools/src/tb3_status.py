32d4739973a786edcfb42f9d4084a0f6c14d96bf
Tom Howard <t.howard@sheffield.ac.uk>
2023-03-23 12:17:17 +0000

    hostname and beep on startup!

--- tuos_tb3_tools/src/tb3_status.py
+++ tuos_tb3_tools/src/tb3_status.py
@@ -2,7 +2,11 @@
 
 import rospy
 import rosnode
+from socket import gethostname
 import datetime as dt
+from turtlebot3_msgs.msg import Sound
+
+hostname = gethostname()
 
 core_nodes = [
     '/turtlebot3_core', 
@@ -18,9 +22,12 @@ class tb3Status():
     def __init__(self):
         self.node_name = "tb3_status"
         self.startup = True
+        self.active_nodes = []
         
         rospy.init_node(self.node_name, anonymous=True)
         self.rate = rospy.Rate(1) # hz
+
+        self.beeper = rospy.Publisher("/sound", Sound, queue_size = 10)
                 
         self.ctrl_c = False
         rospy.on_shutdown(self.shutdownhook) 
@@ -34,17 +41,27 @@ class tb3Status():
 
     def shutdownhook(self):
         self.ctrl_c = True
+    
+    def check_active_nodes(self):
+        self.active_nodes = rosnode.get_node_names()
+        return True if all([i in self.active_nodes for i in core_nodes]) else False
 
     def main(self):
         timestamp = rospy.get_time()
         while self.startup:
             continue
         
+        if self.check_active_nodes():
+            msg = Sound()
+            msg.value = 1
+            print(f"{hostname} is up and running!")
+            self.beeper.publish(msg)
+        
         while not self.ctrl_c:
             if (rospy.get_time() - timestamp) > 10:
-                active_nodes = rosnode.get_node_names()
+                
                 ts = dt.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-                if all([i in active_nodes for i in core_nodes]):
+                if self.check_active_nodes():
                     rts = rospy.get_time() - self.starttime
                     if rts > 60:
                         runtimestring = f"{rts / 60:5.1f}"
@@ -53,9 +70,10 @@ class tb3Status():
                         runtimestring = f"{rts:5.0f}"
                         minsecs="seconds"
                     print(
-                        f"[{ts}] Waffle Status:    OK\n"
-                        f"                       Nodes Active: {len(active_nodes):5d}\n"
-                        f"                  Up Time ({minsecs}): {runtimestring}"
+                        f"[{ts}] Waffle:    {hostname}\n"
+                        f"                      Status:    OK\n"
+                        f"                Nodes Active: {len(self.active_nodes):5d}\n"
+                        f"           Up Time ({minsecs}): {runtimestring}"
                         )
                 else:
                     print(
